3ca35fb1097049ab5e3bdefe086af4393af561f0
Florian <Flori@n-pfaff.de>
2011-09-24 15:45:57 +0200

    periods solves

--- src/main/scala/de/euler/task26/Task26.scala
+++ src/main/scala/de/euler/task26/Task26.scala
@@ -2,27 +2,40 @@ package de.euler.task26
 
 class Task26{
 
-  class DnR(val digits:List[Int],remainder:Int){}
+  class DnR(val digits:List[Int],val remainder:Int){
+    override def toString : String = {
+      return "DnR " + digits + " " + remainder
+    }
+  }
 
-  def findPeriod(prev:List[DnR]): List[Int] = {
+  def findPeriod(prev:List[DnR],d:Int): List[Int] = {
     def makeDivable(z:Int,d:Int):(Int,Int) = {
       if(z<d){
 	val result = makeDivable(z*10,d)
 	return (result._1, result._2+1)
-       }
-       return (z,0)
+      }
+      return (z,0)
     }
-    if(prev.head.remainder == 0) return List()
-    if(prev.tail.exists(_.remainder == prev.head.remainder )){
-      val index = prev.tail.indexOf(prev.head.remainder)
-      return  prev.tail.slice(0,index).flatten
-    } else {
-      val divs = makeDivable(z,d)
-      return findPeriod(new DnR(List.iterate(0,divs._2)(_)::: List(divs._1/d),divs._1%d) ::prev)
+    var remainder = 1
+    if(prev != Nil){
+      val index = if(prev!= Nil)prev.tail.indexWhere(_.remainder == prev.head.remainder ) else -1      
+      if(index > -1){
+	println("found " + prev.head.remainder + " " + index +  " " + prev  + " " + prev.tail.slice(0,index+1))
+	return  prev.tail.slice(0,index +1).map(_.digits).reverse.flatten
+      }
+      remainder = prev.head.remainder
     }
+    val divs = makeDivable(remainder,d)
+    println("divs:" + divs)
+    val newDigits = (divs._1/d).toString.toSeq.map(_.asDigit).toList
+    println("newDigits" + newDigits)
+    val leadingZeros = for(i <- newDigits.length to (divs._2-1)) yield 0
+    println("lz" + leadingZeros)
+    val zDigits = (leadingZeros.toList ::: newDigits)
+    return findPeriod(new DnR(zDigits,divs._1%d) ::prev,d)
   }
 
   def period(n:Int):List[Int] = {
-    
+    return findPeriod(Nil,n)
   }
 }
